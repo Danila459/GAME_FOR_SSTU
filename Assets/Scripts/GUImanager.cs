68509c0655ff94ac3679aae2349ae72a0d4d5d3f
Danila <Dymshakov.Danila@yandex.ru>
2023-06-02 13:01:27 +0400

    change

---
+++ Assets/Scripts/GUImanager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class GUImanager : MonoBehaviour
+{
+    public void Play()
+    {
+      SceneManager.LoadScene("Level");
+    }
+    public void Exit()
+    {
+      Application.Quit();
+    }
+    public void OnMenu()
+    {
+      SceneManager.LoadScene(0);
+    }
+
+    public void Setting()
+    {
+      SceneManager.LoadScene("Setting");
+    }
+}
